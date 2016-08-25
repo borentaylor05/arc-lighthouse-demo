7b631fea647ff1415cbb081aa226402d57d8d7ae
Taylor Boren <taylor.boren@starz.com>
2016-08-24 19:32:51 -0600

    Added check tracker

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/blackout', function (req, res) {
   res.sendFile(__dirname + '/public/arc/blackout-viewer.html');
 });
 
+app.get('/check-tracker', function (req, res) {
+  res.sendFile(__dirname + '/public/arc/check-tracker.html');
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
