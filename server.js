b0eb87e511b99e0226ad8826c0239ced7d251f1a
Taylor Boren <taylor.boren@starz.com>
2016-08-24 18:47:17 -0600

    server workign

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/blackout', function (req, res) {
+  res.sendFile(__dirname + '/public/arc/blackout-viewer.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
